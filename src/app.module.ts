157b5bfcf7d25458bd6d382bc6eebf1bdab7e76a
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-02-17 17:02:56 +0500

    finished getting data from user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,30 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
-import { AdminModule } from './admin/admin.module';
-import { AuthModule } from './auth/auth.module';
-import { CatModule } from './cat/cat.module';
-import { OwnerModule } from './owner/owner.module';
+import { AdminModule } from "./admin/admin.module";
+import { AuthModule } from "./auth/auth.module";
+import { CatModule } from "./cat/cat.module";
+import { OwnerModule } from "./owner/owner.module";
+import { BotModule } from "./bot/bot.module";
+import { TelegrafModule } from "nestjs-telegraf";
+import { BOT_NAME } from "./app.constants";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ".env",
-      isGlobal:true
+      isGlobal: true,
+    }),
+    TelegrafModule.forRoot({
+      botName: BOT_NAME,
+      token: process.env.BOT_TOKEN!,
     }),
     MongooseModule.forRoot(process.env.MONGO_URI!),
     AdminModule,
     AuthModule,
     CatModule,
-    OwnerModule
+    OwnerModule,
+    BotModule,
   ],
   controllers: [],
   providers: [],
